589f88396baa8ae309682236f17ddca9a968a804
wangli <826810835@qq.com>
2017-04-13 17:10:30 +0800

    bootstrap

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,6 +13,24 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
-//= require_tree .
 //= require bootstrap.min
-//= require admin-lte
+//= require adminlte
+
+//= require plugins/slimScroll/jquery.slimscroll
+//= require plugins/iCheck/icheck
+//= require plugins/bootstrap-slider/bootstrap-slider
+//= require plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min
+//= require plugins/chartjs/Chart.min
+//= require plugins/colorpicker/bootstrap-colorpicker.min
+//= require plugins/daterangepicker/moment.min
+//= require plugins/daterangepicker/daterangepicker
+//= require plugins/fastclick/fastclick.min
+//= require plugins/pace/pace.min
+//= require plugins/morris/morris.min
+//= require plugins/select2/select2.min
+//= require plugins/slimScroll/jquery.slimscroll.min
+//= require plugins/sparkline/jquery.sparkline.min
+//= require plugins/timepicker/bootstrap-timepicker.min.js
+
+
+//= require_tree .
